5d110d67b126e57da75dd7fca94677d667d6409d
KareemEzzat <125467138+KareemEzzat91@users.noreply.github.com>
2026-09-08 19:07:04 +0300

    refactor: format API state definitions

--- lib/helpers/cubit_helper/api_state.dart
+++ lib/helpers/cubit_helper/api_state.dart
@@ -3,12 +3,18 @@ part of 'api_cubit.dart';
 sealed class ApiState {}
 
 final class ApiInitial extends ApiState {}
+
 final class LoadingState extends ApiState {}
+
 final class SuccessState extends ApiState {}
+
 final class ErrorState extends ApiState {
-  final String error ;
-  ErrorState( this.error);
-}class ApiSelectedPaymentMethod extends ApiState {
+  final String error;
+
+  ErrorState(this.error);
+}
+
+final class ApiSelectedPaymentMethod extends ApiState {
   final int selectedMethodIndex;
 
   ApiSelectedPaymentMethod(this.selectedMethodIndex);
